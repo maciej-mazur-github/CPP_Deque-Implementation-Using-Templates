334e438b13b34b7e1f632180fc68c24c8234961e
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-11-14 20:10:00 +0100

    Create inner struct Node in deque.h

--- Deque Implementation Using Templates/deque.h
+++ Deque Implementation Using Templates/deque.h
@@ -1,1 +1,44 @@
 #pragma once
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <cstdio>
+using namespace std;
+
+
+
+template <class T, int arraySize>
+class Deque;
+
+template <class T, int arraySize>
+ostream& operator<<(ostream& out, Deque<T, arraySize>& arg);
+
+
+
+template <class T, int arraySize>
+class Deque
+{
+	enum ArrayDirection {rightWiseExpandable, leftWiseExpandable};
+
+	struct Node
+	{
+		Node* nextNodePtr;
+		Node* previousNodePtr;
+		int chosenPosition;
+		T* arrayPtr;
+		int arrayDirection = rightWiseExpandable; // 0 means it is expanding right-hand side, 1 means left-hand side
+		int arrayItemsCounter;
+
+		Node(ArrayDirection arrayDirection = rightWiseExpandable)
+		{
+			nextNodePtr = previousNodePtr = nullptr;
+			chosenPosition = arrayItemsCounter = 0;
+			arrayPtr = new T[arraySize];
+
+			for (int i = 0; i < arraySize; i++)
+			{
+				arrayPtr[i] = nullptr;
+			}
+		}
+	};
+};
---
+++ Deque Implementation Using Templates/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <cstdio>
+#include "deque.h"
+using namespace std;
+
+
+
+int main()
+{
+	Deque<int, 5> test;
+}
